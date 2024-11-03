bef22bb933d47e5c004157b691015e88fc98e3ea
burberryonn <swaronworld2@mail.ru>
2024-11-03 15:49:38 +0300

    start 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const tg = window.Telegram.WebApp.initData;
   return (
     <>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
+      {tg}
     </>
   );
 }
