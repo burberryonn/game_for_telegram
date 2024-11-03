2527345e342cfe9a494be43d7a50ece8854bebad
burberryonn <swaronworld2@mail.ru>
2024-11-03 16:25:02 +0300

    start 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  window.Telegram.WebApp.init();
   const [count, setCount] = useState(0);
   const [players, setPlayers] = useState([]);
-  const tg = window.Telegram.WebApp.initData;
+
+  const user = window.Telegram.WebApp.initDataUnsafe;
+  // const telegramUserData = {
+  //   id: "389592101",
+  //   first_name: "Олег",
+  //   last_name: "Дьяконов",
+  //   username: "burberryonn",
+  //   hash: "7389532998:AAGby3TxdbBs1saGQ9kLJd_bwaFzTyOv0Us",
+  // };
 
   return (
     <>
       {/* {`${tg.username}\n\n${tg.first_name}\n\n${tg.last_name}`} */}
-      {tg.user}
+      {user}
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
